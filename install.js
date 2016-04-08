31a0f598997e106879a47de5f5a00a94079399e4
simonhochrein <simon@ministrycrm.com>
2016-04-08 10:51:29 -0500

    Delete install.js

--- install.js
+++
@@ -1,6 +0,0 @@
-var e = require('child_process').execSync;
-if(require('os').platform().indexOf('win')>-1){
-console.log(e('cd '+__dirname+' && npm install',{encoding:'utf-8'}));
-} else {
-console.log(e('cd '+__dirname+' && sudo npm install',{encoding:'utf-8'}));
-}
