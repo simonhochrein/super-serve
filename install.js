61104877b93ac9b37413c03e564a1df48755c2bc
ministrycrm-admin <info@ministrycrm.com>
2016-04-08 10:39:22 -0500

    commit

--- install.js
+++ install.js
@@ -1,5 +1,5 @@
 var e = require('child_process').execSync;
-if(process.platform().indexOf('win')>-1){
+if(require('os').platform().indexOf('win')>-1){
 console.log(e('cd '+__dirname+' && npm install',{encoding:'utf-8'}));
 } else {
 console.log(e('cd '+__dirname+' && sudo npm install',{encoding:'utf-8'}));
